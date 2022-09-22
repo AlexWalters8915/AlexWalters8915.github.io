f7241eb803367a074bd6ee4cab96976321e362df
AlexWalters <111817564+AlexWalters8915@users.noreply.github.com>
2022-09-21 21:08:47 -0400

    javascript changes

---
+++ itis3135/wsOne/first scripts/darkmode.js
@@ -0,0 +1,13 @@
+//change background to a dark color 
+
+       function dark(){
+    document.body.style.backgroundColor = 'black';
+    document.getElementById("body").style.color = 'white';
+    
+}
+       
+    
+
+
+
+
